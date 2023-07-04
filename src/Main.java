d76512059e9351cddfc756e23a4783daa3137108
Aprilmorse <132219025+Aprilmorse@users.noreply.github.com>
2023-07-04 22:27:47 +0300

    m

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,13 @@
 
 public class Main {
     public static void main(String[] args) {
-        int amount = 1100;
-        int mount
-
-        int bonus;
-        int mount = 100;
-        if (amount > 1000) {
-             bonus = amount / 100;
-         } else {
-             bonus = 0;
-         }
-            
+        int amount = 13676;
+        int mile;
+        {
+            mile = 20;
         }
+        int miles = amount / mile;
 
-        System.out.println("Итоговый счёт: "mount + bonus);("Бонусные рубли: "0 + bonus);
+        System.out.println("Итоговые мили: " + miles);
     }
+}
